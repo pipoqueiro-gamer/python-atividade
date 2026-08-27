aabc7e8098d2ead966a47f4a2a56fa8f56598611
davi <davi.ishizuka@escola.pr.gov.br>
2026-08-27 08:40:55 -0300

    tentei

--- primo.py
+++ primo.py
@@ -1,18 +1,30 @@
+import math
+
+# Exemplo de raiz quadrada
+resultado = math.isqrt(25)
+print(resultado)  # Saída: 5.0
+
+
+
+par:int=0
 primo:int=0
-par=0
+
 for c in range (1,1001):
-    if c%2==0:
-        par+=1
-        print(f"{c} par")
-        
+    if c%==0 and 2%==0:
+     par+=1
+     print(f"{c} é par")
     else:
-        print(f"{c}impar")
-
-    if c%c==0 and c%1==0:
+     print(f"{c} impar")
+    for i in range(1,11):
+      divisor=0
+      if c%i==0 :
+       divisor+=1
+      else:
+       print{c}
+      if divisor==2:
         primo+=1
-        print(f"{c} primo")
-        print(f"primos = {primo}")
-    else:
-        print(f"{c}não primo")
-
-
+      print(f"{c} primo")
+      else:
+      print(f"{c}não primo")
+    
+       
